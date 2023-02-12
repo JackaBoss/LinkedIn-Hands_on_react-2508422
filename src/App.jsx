4e5776fffe233082585d44826018ca8a5a2e658d
Jack <jackchew98@gmail.com>
2023-02-12 21:56:59 +0000

    Finished playing around

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-function App() {
+function App() { 
+  const name = 'StarGazers'
   return (
     <hgroup>
-      <h1>Meet the StarGazers</h1>
+      <img src="images/group.svg" alt="StarGrazers Group" />
+        <h1>Meet the <i style={{color:"Red"}}>{name}</i></h1>
       <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+        <button className="outline" onClick={()=> alert('Hello')}></button>
     </hgroup>
   )
 }
